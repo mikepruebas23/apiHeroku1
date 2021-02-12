c288f5a9fb9aaa297528b7d6cc32f26f4755cab4
mikepruebas23 <mikepruebas23@gmail.com>
2021-02-12 08:44:33 -0700

    firstcommit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const puerto = 3000;
+
+app.get('/',(peticion, respuesta) =>{
+    let agenteDeUsuario = peticion.header('user-agente');
+    respuesta.send('La ruta / solicitada con: ' + agenteDeUsuario);
+});
+
+app.get('/pagina', (peticion, respuesta) =>{
+    let rutaDeArchivo = path.join(__dirname, 'plantilla.html');
+    respuesta.sendFile(rutaDeArchivo);
+});
+
+app.get('/hola',(peticion,respuesta)=>{
+    let mascota = {
+        nombre: "Miguel Armenta",
+        edad: 26
+    };
+
+    respuesta.json(mascota);
+});
+
+app.listen(puerto, err => {
+    if (err) {
+        // Aquí manejar el error
+        console.error("Error escuchando: ", err);
+        return;
+    }
+    console.log(`Escuchando en el puerto :${puerto}`);
+});
