de7d1dea61b52e18451962318834b19aec19efbb
mikepruebas23 <mikepruebas23@gmail.com>
2021-02-12 09:24:34 -0700

    requiere http

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const puerto = 3000;
+const http = require('http');
+const port = process.env.PORT || 3000
 
-app.get('/',(peticion, respuesta) =>{
-    let agenteDeUsuario = peticion.header('user-agente');
-    respuesta.send('La ruta / solicitada con: ' + agenteDeUsuario);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
 });
 
-app.get('/pagina', (peticion, respuesta) =>{
-    let rutaDeArchivo = path.join(__dirname, 'plantilla.html');
-    respuesta.sendFile(rutaDeArchivo);
-});
-
-app.get('/hola',(peticion,respuesta)=>{
-    let mascota = {
-        nombre: "Miguel Armenta",
-        apellido: "Armenta Acosta",
-        edad: 26,
-        
-    };
-
-    respuesta.json(mascota);
-});
-
-app.listen(puerto, err => {
-    if (err) {
-        // Aquí manejar el error
-        console.error("Error escuchando: ", err);
-        return;
-    }
-    console.log(`Escuchando en el puerto :${puerto}`);
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
 });
